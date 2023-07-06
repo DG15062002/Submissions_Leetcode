061659f3008bf64de5d5f626efe1bb4e10f2702a
DG15062002 <83827419+DG15062002@users.noreply.github.com>
2023-07-06 13:56:34 +0530

    Time: 8 ms (48.93%) | Memory: 10.7 MB (36.03%) - LeetSync

--- combination-sum-ii/combination-sum-ii.cpp
+++ combination-sum-ii/combination-sum-ii.cpp
@@ -1,57 +1,31 @@
 class Solution {
 public:
- void solve(int index,vector<int> &candidates, int target, vector<vector<int>> &ans, vector<int> row){
-        if(index >= candidates.size()){
-            if(target == 0)
-            ans.push_back(row);
+  vector<vector<int>> result;
+void comsum(vector<int> &curr, int target, int sum, vector<int> &candidates, int curInd, int n){
+        if(target == sum){
+            result.push_back(curr);
             return;
         }
-        if(candidates[index] <= target){
-            row.push_back(candidates[index]);
-            solve(index+1,candidates,target-candidates[index],ans,row);
-            row.pop_back();
+        else if(sum>target){
+            return;
         }
-        solve(index+1,candidates,target,ans,row);
+        
+        for(int i = curInd; i < n; i++){
+            if(i != curInd && candidates[i]==candidates[i-1])               //to avoid picking up the same combnations i.e. we don't pick same element for certain kth position of a combination 
+                continue;
+            sum += candidates[i];
+            curr.push_back(candidates[i]);
+            comsum(curr, target, sum, candidates, i+1, n);
+            sum -= candidates[i];
+            curr.pop_back();
+        }
+        
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>>  ans,res;vector<int> row;
-        map<int,int> mp1;
-        int sum = 0;
-        for(auto i: candidates){
-            sum+=i;
-            mp1[i]++;
-        }
-        if(sum < target) return ans;
-        if(mp1.size() == 1){
-            if(target % candidates[0] == 0){
-                for(int i = 0; i < target/ candidates[0]; i++)
-                row.push_back(candidates[i]);
-            ans.push_back(row);
-            }
-            return ans;
-        }
-        if(mp1.size() == 2 && mp1[2] == 1 && mp1[1] == candidates.size()-1  && target == 30){
-            for(int i = 0; i< 30; i++){
-                row.push_back(1);
-            }
-            ans.push_back(row);
-            row.pop_back();
-            row.pop_back();
-            row.push_back(2);
-            ans.push_back(row);
-            return ans;
-        }
-        solve(0, candidates, target, ans, row);
-        if(ans.empty()) return ans;
-        int n = ans.size();
-         map<vector<int>,int> mp;
-        for(int i = 0; i < n; i++){
-            vector<int> temp = ans[i];
-            sort(temp.begin(),temp.end());
-            mp[temp]++;
-            if(mp[temp] == 1)
-            res.push_back(temp);
-        }
-        return res;
+        vector<int> curr;
+        int n = candidates.size();
+        sort(candidates.begin(), candidates.end());
+        comsum(curr, target, 0, candidates, 0, n);
+        return result;
     }
 };
