878c99a1d5ce8c355217688ba6779250d2e9acd1
DG15062002 <83827419+DG15062002@users.noreply.github.com>
2023-07-16 10:17:20 +0530

    Time: 56 ms (47.87%) | Memory: 15.9 MB (82.19%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -42,9 +42,9 @@ bool isValid(int i, int j, vector<vector<int>> &grid){
         int ans = 0;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-            ans = max(ans,solve(i,j,matrix,dp));
+            ans = max(ans,1+solve(i,j,matrix,dp));
         }
         }
-        return ans+1;
+        return ans;
     }
 };
