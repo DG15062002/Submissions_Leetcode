06992151eef0ed72f47665b7372515796216fe39
DG15062002 <83827419+DG15062002@users.noreply.github.com>
2023-07-19 20:49:21 +0530

    Time: 465 ms (92.24%) | Memory: 109 MB (6.38%) - LeetSync

---
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+struct Interval {
+    int start;
+    int end;
+    Interval(int s, int e) : start(s), end(e) {}
+};
+
+static bool compareIntervals(Interval& a, Interval& b) {
+    return a.end < b.end;
+}
+
+int maxNonOverlappingIntervals(vector<Interval>& intervals) {
+    if (intervals.empty()) return 0;
+
+    // Sort intervals based on ending points
+    sort(intervals.begin(), intervals.end(), compareIntervals);
+
+    int count = 1; // Count of non-overlapping intervals
+    int lastEnding = intervals[0].end; // Ending point of the last selected interval
+
+    for (int i = 1; i < intervals.size(); i++) {
+        if (intervals[i].start > lastEnding) {
+            count++;
+            lastEnding = intervals[i].end;
+        }
+    }
+
+    return count;
+}
+
+    int findMinArrowShots(vector<vector<int>>& points) {
+        int n = points.size();
+        vector<Interval> v;
+        for(auto i : points){
+            v.push_back({i[0],i[1]});
+        }
+        return maxNonOverlappingIntervals(v); 
+    }
+};
