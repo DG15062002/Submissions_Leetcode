37e710e67f5c834351acf0fa25b39bd9611ca370
DG15062002 <83827419+DG15062002@users.noreply.github.com>
2023-07-24 08:42:01 +0530

    Time: 5 ms (6.57%) | Memory: 5.9 MB (92.36%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    double solve(double x, long long n){
+        double ans = 1;
+        while(n > 0){
+            int c = n & 1;
+            if(c == 1){
+                ans = ans*x;
+            }
+            x = x*x;
+            n = n >> 1;
+        }
+        return ans;
+    }
+    double myPow(double x, int n) {
+    if(x == 1) return 1;
+    double ans = solve(x,abs(n));
+    if(n < 0)
+    return 1/ans;
+    cout << INT_MAX;
+    return ans;
+    }
+};
