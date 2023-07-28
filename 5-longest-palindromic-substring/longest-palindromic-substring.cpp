bc7a7d2c328a6899c46e2a8ee2f870e41da8ec11
DG15062002 <83827419+DG15062002@users.noreply.github.com>
2023-07-28 07:37:29 +0530

    Time: 11 ms (94.70%) | Memory: 11.3 MB (55.05%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,39 +1,23 @@
 class Solution {
-private: 
-    bool solve(vector<vector<bool>> &dp, int i, int j, string &s){
-        if(i == j){
-            return dp[i][j] = true;
-        }
-        if(j-i == 1){
-            if(s[i] == s[j]){
-                return dp[i][j] = true;
-            }
-            else{
-                return dp[i][j] = false;
-            }
-        }
-        if(s[i] == s[j] && dp[i+1][j-1] == true){
-            return dp[i][j] = true;
-        } else {
-            return dp[i][j] = false;
-        }
-    }
 public:
-    string longestPalindrome(string s) {
-        int n = s.size();
-        int startIndex = 0; int maxlen = 0;
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
-        for(int g=0; g<n; g++){
-            for(int i=0, j=g; j<n; i++, j++){
-                solve(dp, i, j, s);
-                if(dp[i][j] == true){
-                    if(j-i+1 > maxlen){
-                        startIndex = i;
-                        maxlen = j-i+1;
-                    }
-                }
-            }
+string ans = "";
+    void expand(string &s , int left ,int right)
+    {
+        while(left >= 0 &&  right < s.size())
+        {
+            if(s[left] != s[right])
+                break;
+            left--,right++;
         }
-        return s.substr(startIndex, maxlen);
+        if(ans.size() < right - left )
+            ans = s.substr(left + 1 , right - left - 1);
+    }
+    string longestPalindrome(string s) {
+         for(int i = 0 ; i < s.size() ; i++)
+        {
+            expand(s , i , i);
+            expand(s , i , i+1);
+        }
+        return ans;
     }
 };
